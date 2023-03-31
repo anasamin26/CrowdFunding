286a088d5ebab79fdbcfcd368fe949bd2c5cb830
Anas <anasamin26@gmail.com>
2023-04-01 03:37:15 +0500

    index.js -home styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import { ConnectWallet } from "@thirdweb-dev/react";
-import styles from "../styles/Home.module.css";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import Home from "./Home";
 import Profile from "./Profile";
